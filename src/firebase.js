97fda41f65015a7eca2ce58fb89e6aa29c118532
Florent Cuer <florentcuer@gmail.com>
2024-11-11 15:18:30 +0000

    moved google api key

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import 'secrets.dart';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: googleApiKey,
+  authDomain: "chat-app-2869a.firebaseapp.com",
+  projectId: "chat-app-2869a",
+  storageBucket: "chat-app-2869a.firebasestorage.app",
+  messagingSenderId: "116627457786",
+  appId: "1:116627457786:web:f301cc7a105373c0506b43"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
